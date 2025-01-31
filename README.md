7444d3924a20025841c4940fd5d890df70365e8f
Grace Matsuoka <gracelmatsuoka@gmail.com>
2025-01-30 22:07:20 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## iMovie 🍿
+
+🚧 This application is still under construction 🚧
+
+iMovie is a web application using React, Node.js, and public APIs to create a dynamic movie database/platform. Users will be able to find any movie they wish and look at its corresponding details. Users will also be able to save movies in their 'favorites' section to revisit later. 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import MovieCard  from "../components/MovieCard";
+import {useState} from "react"
+
+function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const movies = [
+        {id: 1, title: "hi", release_date: "2004"},
+        {id: 2, title: "hi", release_date: "2004"},
+        {id: 3, title: "hi", release_date: "2004"}
+    ]
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+    }
+
+    return (
+    <div className="home">
+        <form onSubmit = {handleSearch} className="search-form">
+            <input 
+                type="text" 
+                placeholder="Search for movies..." 
+                className="search-input"
+                value = {searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button type="submit" className="search-button">Search</button>
+        </form>
+        <div className="movies-grid">
+            {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id}/>
+            ))}
+        </div>
+    </div>)
+}
+
+export default Home
